4c4683df5ce768f544ab0a67dc3b7272e63bbe3f
Mitja Poljsak <mfsnoop@gmail.com>
2014-08-11 14:55:27 +0200

    Custom json body for post requests

--- src/Knp/FriendlyContexts/Builder/PostRequestBuilder.php
+++ src/Knp/FriendlyContexts/Builder/PostRequestBuilder.php
@@ -11,6 +11,12 @@ class PostRequestBuilder extends AbstractRequestBuilder
 
         $resource = $queries ? sprintf('%s?%s', $uri, $this->formatQueryString($queries)) : $uri;
 
+        //set postBody to body if postBody is not set
+        //useful for custom json body POST requests
+        if (null === $postBody && null !== $body) {
+            $postBody = $body;
+        }
+
         return $this->client->post($resource, $headers, $postBody, $options);
     }
 }
